5165ad027da557304d79c444162db8c5a0243c8a
Ayush-Gole8 <ayushgole1910@gmail.com>
2026-01-14 22:49:32 +0530

    update

--- Contest/Solution2183I1.java
+++ Contest/Solution2183I1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class Solution2183I1 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(System.out);
