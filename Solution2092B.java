d3d947411b1f563054738ae26873e9c4ec51e687
Ayush-Gole8 <ayushgole1910@gmail.com>
2026-01-28 20:51:22 +0530

    Done

---
+++ Solution2092B.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution2092B {
+    static FastScanner fs = new FastScanner();
+    static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+    public static void main(String[] args) {
+        int t = fs.nextInt(); 
+        while (t-- > 0) {
+            solve();
+        }
+        out.flush();
+    }
+
+    static void solve() {
+    int n = fs.nextInt();
+    String a = fs.next();
+    String b = fs.next();
+    int ones1 = 0, ones2 = 0;
+    for (int i = 0; i < n; i++) {
+        if ((i & 1) == 0) { 
+            if (a.charAt(i) == '1') ones1++; 
+            if (b.charAt(i) == '1') ones2++; 
+        } else {       
+            if (b.charAt(i) == '1') ones1++; 
+            if (a.charAt(i) == '1') ones2++; 
+        }
+    }
+    int bPos1 = n / 2;         
+    int bPos2 = (n + 1) / 2;  
+    if (ones1 <= bPos1 && ones2 <= bPos2) {
+        out.println("YES");
+    } else {
+        out.println("NO");
+    }
+}
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() {
+            try {
+                if (ptr >= len) {
+                    len = in.read(buffer);
+                    ptr = 0;
+                    if (len <= 0) return -1;
+                }
+                return buffer[ptr++];
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        String next() {
+            StringBuilder sb = new StringBuilder();
+            int c;
+            while ((c = read()) <= 32) {
+                if (c == -1) return null;
+            }
+            do {
+                sb.append((char) c);
+            } while ((c = read()) > 32);
+            return sb.toString();
+        }
+
+        int nextInt() {
+            int c = read();
+            while (c <= 32) c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int val = 0;
+            while (c > 32) {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sgn;
+        }
+
+        long nextLong() {
+            int c = read();
+            while (c <= 32) c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long val = 0;
+            while (c > 32) {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sgn;
+        }
+
+        int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+    }
+}
