0377a7876eaf69b79509288ed112ad0d8bb46125
Ayush-Gole8 <ayushgole1910@gmail.com>
2026-04-13 22:33:37 +0530

    2_more

---
+++ Solution1853A.java
@@ -0,0 +1,114 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution1853A {
+    static FastScanner fs = new FastScanner();
+    static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
+
+    public static void main(String[] args) {
+        int t = fs.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        out.flush();
+    }
+
+    static void solve() {
+        int n = fs.nextInt();
+        int[] a = fs.nextIntArray(n);
+        for (int i = 1; i < n; i++) {
+            if (a[i] < a[i - 1]) {
+                out.println(0);
+                return;
+            }
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            int diff = a[i] - a[i - 1];
+            int ops = diff / 2 + 1;
+            ans = Math.min(ans, ops);
+        }
+        out.println(ans);
+    }
+
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() {
+            try {
+                if (ptr >= len) {
+                    len = in.read(buffer);
+                    ptr = 0;
+                    if (len <= 0)
+                        return -1;
+                }
+                return buffer[ptr++];
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public long[] nextLongArray(long n) {
+            long[] a = new long[(int) n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextLong();
+            return a;
+        }
+
+        String next() {
+            StringBuilder sb = new StringBuilder();
+            int c;
+            while ((c = read()) <= 32) {
+                if (c == -1)
+                    return null;
+            }
+            do {
+                sb.append((char) c);
+            } while ((c = read()) > 32);
+            return sb.toString();
+        }
+
+        int nextInt() {
+            int c = read();
+            while (c <= 32)
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int val = 0;
+            while (c > 32) {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sgn;
+        }
+
+        long nextLong() {
+            int c = read();
+            while (c <= 32)
+                c = read();
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long val = 0;
+            while (c > 32) {
+                val = val * 10 + (c - '0');
+                c = read();
+            }
+            return val * sgn;
+        }
+
+        int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+    }
+}
